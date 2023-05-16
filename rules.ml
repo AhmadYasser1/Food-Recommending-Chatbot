a301f1728d85ae0820de3d47053e1952bd3affb5
Ahmad Yasser <ahmadnis123@gmail.com>
2023-05-16 08:20:37 +0300

    Update rules.ml & Add new rules

--- rules.ml
+++ rules.ml
@@ -1,13 +1,15 @@
 (* Define a set of rules to base the program upon *)
 type rules = 
 | Greeting of string
+| Recommendation of string
 | Question of string
 | Hunger of string
 | Cuisine of string
-| Indoors_Outdoors of string 
-| Food_Recommendation of string
-| Closing_Conversation of string
+| Dining of string
+| Indoors_Outdoors of string
 | Delivery of string
+| Takeaway of string 
 | Restaurant_Location of string
 | Price_Range of string
+| Closing_Conversation of string
 ;;
