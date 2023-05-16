33997c75cd5d4a77a80d34f760084e873b17190d
Ahmad Yasser <ahmadnis123@gmail.com>
2023-05-16 08:19:50 +0300

    Update lex.ml (*Pending: Takes Input Only*)

--- lex.ml
+++ lex.ml
@@ -36,8 +36,8 @@ let isDigit digit =
   int_index_option <> None
 ;;
 
-(* Reads the dictionary.csv file as a string and converts it to a String List List  *)
-let convert_dictionary_csv_to_stringListList =
+(* Reads the dictionary.csv file as a string and converts it to a String List List *)
+let dictionary_stringListList =
   let dictionary_file_from_csv = open_in "Dictionary.csv" in
   let rec loop_over_lines accumulator =
     try
@@ -56,7 +56,7 @@ let string_to_charList string =
 ;;
 
 (* Removes the duplicated characters in a Char List *)
-let removeDuplicatesFromCharList char_list =
+let removeDuplicatesInCharList char_list =
   let rec helper char_list accumulator =
   match char_list with
   | [] -> accumulator
@@ -65,19 +65,17 @@ let removeDuplicatesFromCharList char_list =
   helper char_list []
 ;;
 
-(* Computes a string with no duplicated characters *)
-let removeDuplicatesFromString userInput = 
-  (* String.make is used to create a string of length 1 for it to be concatenated with the accumulator
-   * List.rev is used for the list to be compatible with the fold_left method *)
-  List.fold_left (fun accumulator char -> accumulator ^ (String.make 1 char)) "" (List.rev (removeDuplicatesFromCharList (string_to_charList userInput)))
+(* Removes the duplicated characters in a String *)
+let removeDuplicatesInString userInput = 
+  List.fold_left (fun accumulator char -> accumulator ^ (String.make 1 char)) "" (List.rev (removeDuplicatesInCharList (string_to_charList userInput)))
 ;;
 
 (* Result of the convert_dictionary_csv_to_stringListList *)
-let dictionary_stringListList = convert_dictionary_csv_to_stringListList;;
+(* let dictionary_stringListList = convert_dictionary_csv_to_stringListList;; *)
 
-(* Maps the dictionary file to a string list list *)
-let filterDictionaryByKeyword dictionary_file keyword =
-  List.map (fun string_list -> List.filter (fun word -> removeDuplicatesFromString(word) = removeDuplicatesFromString(keyword)) string_list) dictionary_file
+(* Filters the dictionary *)
+let filterDictionaryByKeyword dictionary_stringListList keyword =
+  List.map (fun string_list -> List.filter (fun word -> removeDuplicatesInString(word) = removeDuplicatesInString(keyword)) string_list) dictionary_stringListList
 ;;
 
 (* Maps the string list list to a string list *)
@@ -86,92 +84,76 @@ let map_stringListList_to_stringList dictionary_stringListList keyword =
     fun word -> match word with
       | [] | _::_::_ -> ""
       | [word] -> word) (filterDictionaryByKeyword dictionary_stringListList keyword)
-    ;;
-
+;;
 (* Gets the index of the keyword found in the filtered list *)
 let get_index_of_keyword filtered_list =
- let rec helper filtered_list position =
-   match filtered_list with
-   | keyword :: _ when (String.length keyword > 0)-> position
-   | _ :: tail -> helper tail (position + 1)
-   | [] -> position
- in
- helper filtered_list 1
-;;
-
+  let rec helper filtered_list index =
+    match filtered_list with
+    | keyword :: _ when (String.length keyword > 0) -> index
+    | _ :: tail -> helper tail (index + 1)
+    | [] -> index
+  in
+  helper filtered_list 1
+ ;;
 (* Lexes the user input (explained thoroughly below) *)
 let rec lexString userInput =
   let string_length = String.length userInput in
-  let rec tokenizeString position =
-    if position >= string_length then
-    []
+  let rec tokenizeString starting_index accumulator =
+    if starting_index >= string_length then
+    accumulator
   else
-    match userInput.[position] with
+    match userInput.[starting_index] with
     (* Ignore tabs, spaces, new lines, and  punctuations *)
-    | ' ' | '\t' | '\n' | '?' | '!' | '.' -> tokenizeString (position + 1)
+    | ' ' | '\t' | '\n' | '?' | '!' | '.' -> tokenizeString (starting_index + 1) (accumulator)
     (* If the lexer meets a character of type Char it takes all 
      * characters after it until the next character is not of type Char *)
     | char when (isChar char) ->(
-        let stopping_index = ref position in
+        let stopping_index = ref starting_index in
         while !stopping_index < string_length && isChar (Char.lowercase_ascii userInput.[!stopping_index]) do
         incr stopping_index;
         done;
         (* keyword is saved as a substring of the original string from 
          *the calcaulated index above *)
-        let keyword = removeDuplicatesFromString(String.lowercase_ascii (String.sub userInput position (!stopping_index - position))) in
+        let keyword = removeDuplicatesInString(String.lowercase_ascii (String.sub userInput starting_index (!stopping_index - starting_index))) in
         (* maps the keyword to the rules in the dictionary to identify 
-         *it falls under which rule while removing the duplicates 
-         * Example: heeyy and helloo are validated*)
-        let keyword_index =  map_stringListList_to_stringList (filterDictionaryByKeyword dictionary_stringListList userInput) (removeDuplicatesFromString(userInput)) |> get_index_of_keyword in
+         * it falls under which rule while removing the duplicates 
+         * Example: heeyy and helloo are validated *)
+        let keyword_index =  get_index_of_keyword (map_stringListList_to_stringList (filterDictionaryByKeyword dictionary_stringListList keyword) (removeDuplicatesInString(keyword))) in
         match keyword_index with
-        | 1 -> Greeting (keyword) :: tokenizeString(!stopping_index)
-        | 2 -> Question (keyword) :: tokenizeString (!stopping_index)
-        | 3 -> Hunger (keyword) :: tokenizeString (!stopping_index)
-        | 4 -> Cuisine (keyword) :: tokenizeString (!stopping_index)
-        | 5 -> Indoors_Outdoors (keyword) :: tokenizeString (!stopping_index)
-        | 6 -> Food_Recommendation (keyword) :: tokenizeString (!stopping_index)
-        | 7 -> Closing_Conversation (keyword) :: tokenizeString (!stopping_index)
-        | 8 -> Delivery (keyword) :: tokenizeString (!stopping_index)
-        | 9 -> Restaurant_Location (keyword) :: tokenizeString (!stopping_index)
-        | _ -> tokenizeString (!stopping_index))
+        | 1 -> tokenizeString(!stopping_index) (Greeting (keyword) :: accumulator)
+        | 2 -> tokenizeString(!stopping_index) (Recommendation (keyword) :: accumulator)
+        | 3 -> tokenizeString(!stopping_index) (Question (keyword) :: accumulator)
+        | 4 -> tokenizeString(!stopping_index) (Hunger (keyword) :: accumulator)
+        | 5 -> tokenizeString(!stopping_index) (Cuisine (keyword) :: accumulator)
+        | 6 -> tokenizeString(!stopping_index) (Dining (keyword) :: accumulator)
+        | 7 -> tokenizeString(!stopping_index) (Indoors_Outdoors (keyword) :: accumulator)
+        | 8 -> tokenizeString(!stopping_index) (Delivery (keyword) :: accumulator)
+        | 9 -> tokenizeString(!stopping_index) (Takeaway (keyword) :: accumulator)
+        | 10 -> tokenizeString(!stopping_index) (Restaurant_Location (keyword) :: accumulator)
+        | 11 -> tokenizeString(!stopping_index) (Price_Range (keyword) :: accumulator)
+        | 12 -> tokenizeString(!stopping_index) (Closing_Conversation (keyword) :: accumulator)
+        | _ -> tokenizeString (!stopping_index) (accumulator))
     (* If the lexer meets a character of type int it takes all 
-     * characters after it until the next character is not of type int *)
+    * characters after it until the next character is not of type int *)
     | digit when (isDigit digit) ->
-          (let stopping_index = ref position in
+          (let stopping_index = ref starting_index in
           while !stopping_index < string_length && isDigit userInput.[!stopping_index] do
-          incr stopping_index;
+            incr stopping_index;
           done;
           (* Any number computed is saved in as string and will 
            * represent the Price Range rule *)
-          let price_string = String.sub userInput position (!stopping_index - position) in
+          let price_string = String.sub userInput starting_index (!stopping_index - starting_index) in
           (* Price is converted to an int *)
           let price = int_of_string price_string in
           match price with
           | 0 -> printf "Food isn't free buddy. Food isn't free. Please enter a reasonable amount:\n\n"; getUserInput () |> lexString
-          | price when price >= 5 && price <= 200 -> Price_Range("$") :: (tokenizeString !stopping_index) 
-          | price when price >= 201 && price <= 500 -> Price_Range("$$") :: (tokenizeString !stopping_index)
-          | price when price >= 501 -> Price_Range("$$$") :: (tokenizeString !stopping_index)
-          | _ -> tokenizeString (!stopping_index) )
-    | _ -> tokenizeString (position + 1)
+          | price when price >= 5 && price <= 200 -> tokenizeString(!stopping_index) (Price_Range("$") :: accumulator)
+          | price when price >= 201 && price <= 500 -> tokenizeString(!stopping_index) (Price_Range("$$") :: accumulator)
+          | price when price >= 501 -> tokenizeString(!stopping_index) (Price_Range("$$$") :: accumulator)
+          | _ -> tokenizeString (!stopping_index) (accumulator))
+    | _ -> tokenizeString (starting_index + 1) (accumulator)
   in
-  tokenizeString 0
-;;
-
-(* Generates response according to the user's response *)
-let rec generateResponse list_of_tokens list_of_responses =
-  match list_of_tokens with
-  | Question _ :: tail -> generateResponse tail (list_of_responses)
-  | Greeting _ :: _ -> printf "%s" greetUserResponse ; generateResponse (getUserInput () |> lexString) (list_of_responses)
-  | Hunger _ :: _ -> printf "\nOkay, What are you in the mood to eat?\n\n"; generateResponse (getUserInput () |> lexString) (list_of_responses)
-  | Food_Recommendation _ :: _ -> printf "\nWell there's burger, fried chicken, sushi, beef, steak, shawerma and koshary, 
-Choose your pick.\n\n"; generateResponse (getUserInput () |> lexString) (list_of_responses)
-  | Cuisine head :: _ ->  printf "\nOhh nice choice. Okay what's the location where you'll eat?\n\n"; generateResponse (getUserInput () |> lexString) (list_of_responses @ [head])
-  | Restaurant_Location head :: _ -> printf "\nOkay that's a nice place. Do you want to dine (indoors, outdoors or do you want it delivery)?\n\n"; generateResponse (getUserInput () |> lexString) (list_of_responses @ [head])
-  | Indoors_Outdoors head :: _ -> printf "\nI like your picks. Okay now what's ur price range?\n\n"; generateResponse (getUserInput () |> lexString) (list_of_responses @ [head])
-  | Price_Range head :: _ -> printf "\nOooh that's gonna be cutting it. Okay let me process things and I'll get to you in a moment\n\n"; (list_of_responses @ [head])
-  | Closing_Conversation _ :: _ -> printf "\nHope to see you again!!\n\n"; list_of_responses
-  | [] -> printf "\nI didn't quite understand that. Could you come again please.\n\n"; generateResponse (getUserInput () |> lexString) (list_of_responses)
-  | _ -> []
+  tokenizeString 0 []
 ;;
 
 (* Greets the user as the first message of the program *)
@@ -180,5 +162,4 @@ print_string "\nHello User\n
           But first I need to ask you some stuff. *RUMBLING* HAHA Looks like you're dying to get a bite to eat. Let's get into it.
           How can i help you???\n\n";;
 
-
-let userPreference = generateResponse (getUserInput () |> lexString) [];;
+let userPreference = getUserInput () |> lexString ;;
